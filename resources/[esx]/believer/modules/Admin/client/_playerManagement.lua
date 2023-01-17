64489da1f462adcb45d9c4682baa1a818c46031e
Joshua <101841420+JoshuaSL@users.noreply.github.com>
2023-01-17 12:17:34 +0100

    Update _playerManagement.lua

--- resources/[esx]/believer/modules/Admin/client/_playerManagement.lua
+++ resources/[esx]/believer/modules/Admin/client/_playerManagement.lua
@@ -4,6 +4,8 @@ GM.Admin.menu.submenus["players"] = RageUI.CreateSubMenu(GM.Admin.menu.main, "",
 
 GM.Admin.menu.submenus["players_online"] = RageUI.CreateSubMenu(GM.Admin.menu.submenus["players"], "", "Joueurs en ligne")
 
+GM.Admin.menu.submenus["players_online_management"] = RageUI.CreateSubMenu(GM.Admin.menu.submenus["players_online"], "", "Joueurs en ligne - Management")
+
 GM.Admin.menu.submenus["players"]:isVisible(function(Items)
     Items:Button("Joueurs en ligne", nil, {}, GM.Admin.inAdmin, {
         onSelected = function()
@@ -16,8 +18,16 @@ GM.Admin.menu.submenus["players_online"]:isVisible(function(Items)
     for playerSrc, playersValues in pairs(GM.Admin.data["players"]) do
         Items:Button("("..playersValues.id..") - "..playersValues.name, nil, { RightLabel = playersValues.rank}, GM.Admin.inAdmin, {
             onSelected = function()
-                -- Todo set current player selected
+                GM.Admin.data["selectedPlayer"] = playerValues.id
             end
-        })
+        }, GM.Admin.menu.submenus["players_online_management"])
     end
+end)
+
+GM.Admin.menu.submenus["players_online_management"]:isVisible(function(Items)
+    Items:Button("Kick", nil, {}, GM.Admin.inAdmin, {
+        onSelected = function()
+            TriggerServerEvent("Admin:kickPlayer", GM.Admin.data["selectedPlayer"])
+        end
+    })
 end)
