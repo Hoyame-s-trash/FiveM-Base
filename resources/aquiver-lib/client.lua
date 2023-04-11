eab2349868adf8dc585129d59b5f82d391652128
Joshua <101841420+JoshuaSL@users.noreply.github.com>
2023-04-11 19:01:39 +0200

    update server

---
+++ resources/aquiver-lib/client.lua
@@ -0,0 +1,14 @@
+---@class AquiverLibClient
+Client = {}
+Client.Classes = {}
+Client.Managers = {}
+
+exports("getClient", function()
+    return Client
+end)
+
+function SendCefMessage(ctx)
+    SendNUIMessage(ctx)
+end
+
+RegisterNetEvent("SEND_CEF_MESSAGE", SendCefMessage)
---
+++ resources/aquiver-lib/exports/client_exports.lua
@@ -0,0 +1,4 @@
+---@type AquiverLibClient
+AquiverClientLibrary = exports["aquiver-lib"]:getClient()
+---@type AquiverLibShared
+AquiverSharedLibrary = exports["aquiver-lib"]:getShared()
