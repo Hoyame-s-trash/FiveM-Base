d39e53b6bda5c3418be4d4a1fece9bc63b2fd6b4
Joshua <101841420+JoshuaSL@users.noreply.github.com>
2023-04-24 20:41:56 +0200

    update

--- resources/inventory/cef/src/plugins/audio.plugin.ts
+++
@@ -1,5 +0,0 @@
-export function playAudio(path: string, volume: number = 0.025) {
-    const d = new Audio(path);
-    d.volume = volume;
-    d.play();
-}
--- resources/inventory/cef/src/types/shopItem.ts
+++
@@ -1,8 +0,0 @@
-import { I_ItemData, I_ItemMetaData } from "./item";
-
-export interface I_ShopItem {
-    name: string;
-    meta: I_ItemMetaData;
-    data: Partial<I_ItemData>;
-    price: number;
-}
